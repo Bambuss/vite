23de0e74a3650357b82ce67ae7d0542146be90ec
Mateusz Gozdek <gozdek.mateusz909@gmail.com>
2022-03-01 20:26:01 +0100

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from "axios"
+
+defineProps({
+  msg: String
+})
+
+
+const items = ref([])
+onMounted(()=> {
+  axios.get('https://jsonplaceholder.typicode.com/todos').then(res => {
+    items.value = res.data;
+  })
+})
+
+
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div v-for="item in items" :key="item.id">
+    {{item.title}}
+  </div>
+
+ 
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
